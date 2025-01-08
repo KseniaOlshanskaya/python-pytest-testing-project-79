fa08bf70fb170fb9d1a7f2baff41480c388617c5
KseniaOlshanskaya <ks14012000@gmail.com>
2025-01-09 02:14:11 +0700

    Fixed main function

--- page_loader/scripts/main.py
+++ page_loader/scripts/main.py
@@ -6,12 +6,11 @@ def main():
     parser = argparse.ArgumentParser(
         description="Page loader Hexlet"
     )
-    parser.add_argument('url', help='URL')
+    parser.add_argument('--url', help='URL')
     parser.add_argument('-o', '--output', help='output directory ')
 
     args = parser.parse_args()
-    file_path =download.download(args)
-
+    file_path = download(args)
     print(file_path)
 
 
