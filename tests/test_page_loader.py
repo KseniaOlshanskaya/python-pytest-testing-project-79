c8f347feada79926b9dccfde5f6db18eecd34380
KseniaOlshanskaya <ks14012000@gmail.com>
2025-01-12 00:34:47 +0700

    Added new tests

--- tests/test_page_loader.py
+++ tests/test_page_loader.py
@@ -4,22 +4,26 @@ from page_loader.download import download
 
 
 
-def test_download(tmp_path):
+def test_file_download(tmp_path):
     temp = str(tmp_path)
     file_path = download(url='https://ru.hexlet.io/courses', output=temp)
-    assert file_path == os.path.join(temp, 'ru-hexlet-io-courses.html')
+    expected_path = os.path.join(temp, 'ru-hexlet-io-courses.html')
+    assert file_path == expected_path
+    assert os.path.exists(expected_path)
 
-def test_file_created(tmp_path):
-    temp = str(tmp_path)
-    file_path = download(url='https://ru.hexlet.io/courses', output=temp)
-    try:
-        with open(file_path, 'r') as file:
-            pass
-    except FileNotFoundError:
-        pass
+def test_output_path_not_exist(tmp_path):
+    unexpected_path = 'файл/'
+    with pytest.raises(FileNotFoundError):
+        download(url='https://ru.hexlet.io/courses', output=unexpected_path)
 
-def test_download_without_output(fs):
+def test_download_without_output():
     file_path = download(url='https://ru.hexlet.io/courses')
-    assert file_path == os.path.join(tmp_path, 'ru-hexlet-io-courses.html')
+    expected_path = (os.path.join(os.path.dirname(__file__), 'ru-hexlet-io-courses.html')
+                     .replace('tests', 'page_loader'))
+    assert file_path == expected_path
+    os.remove(expected_path)
+    assert not os.path.exists(expected_path)
+
+
 
 
