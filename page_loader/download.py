b377ccbaa990184fc94d28be189723897865391b
KseniaOlshanskaya <ks14012000@gmail.com>
2025-01-12 00:35:22 +0700

    Updated download function

--- page_loader/download.py
+++ page_loader/download.py
@@ -9,7 +9,7 @@ def download(url: str, output: str = None):
     file_name = re.sub(r'[^A-Za-z0-9]', '-', file_name) # change symbols to -
     output_folder = output if output else os.path.dirname(__file__)
     full_file_path = os.path.join(output_folder, file_name + '.html')
+    r = requests.get(url)
     with open(full_file_path, "wb") as file:
-        r = requests.get(url)
         file.write(r.content)
     return full_file_path
