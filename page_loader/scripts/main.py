ab8b26956da74ebbf361b5e1357cad66f516e8a8
KseniaOlshanskaya <ks14012000@gmail.com>
2025-02-16 14:04:58 +0700

    Released a new version of the app

--- page_loader/scripts/main.py
+++ page_loader/scripts/main.py
@@ -6,11 +6,15 @@ def main():
     parser = argparse.ArgumentParser(
         description="Page loader Hexlet"
     )
-    parser.add_argument('--url', help='URL')
     parser.add_argument('-o', '--output', help='output directory ')
+    parser.add_argument('url', help='URL')
 
     args = parser.parse_args()
-    file_path = download(url=args.url)
+    if args.output:
+        file_path = download(url=args.url, output=args.output)
+    else:
+        file_path = download(url=args.url)
+    return file_path
 
 
 if __name__ == "__main__":
