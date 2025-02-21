8239821e9f9e5dda07eb67e093009fec19800066
KseniaOlshanskaya <ks14012000@gmail.com>
2025-02-22 02:24:54 +0700

    Added test on checking content of assets

--- tests/test_page_loader.py
+++ tests/test_page_loader.py
@@ -1,3 +1,4 @@
+import difflib
 import os
 import pytest
 from page_loader.download import download
@@ -26,16 +27,20 @@ def test_download_without_output():
     os.remove(expected_path)
     assert not os.path.exists(expected_path)
 
-def test_assets_folder_exists():
+def test_assets_exist():
     download(url='https://ru.hexlet.io/courses')
+    required_files = {"ru-hexlet-io-courses.html", "ru-hexlet-io-manifest.json"}
     expected_assets_dir_path = (os.path.join(os.path.dirname(__file__), 'ru-hexlet-io-courses_files')
                                 .replace('tests', 'page_loader'))
     assert os.path.exists(expected_assets_dir_path)
-    assert os.listdir(expected_assets_dir_path)
+    for file in required_files:
+        file_path = os.path.join(expected_assets_dir_path, file)
+        assert os.path.exists(file_path)
+
 
 def test_assets_href_change():
 
-    with open('fixtures/test_assets_href_change.yaml', 'r') as f:
+    with open('fixtures/assets_tags.yaml', 'r') as f:
         fixt = yaml.load(f, Loader=yaml.SafeLoader)
 
     file_path = download(url='https://ru.hexlet.io/courses')
@@ -50,3 +55,18 @@ def test_assets_href_change():
                     if file_extension in fixt['asset_extensions']:
                         assert 'ru-hexlet-io-courses_files' in root
                         assert 'http' not in root
+
+def test_check_asset_content():
+    download(url='https://ru.hexlet.io/courses')
+    expected_assets_dir_path = (os.path.join(os.path.dirname(__file__), 'ru-hexlet-io-courses_files')
+                                .replace('tests', 'page_loader'))
+    existing_files = set(os.listdir(expected_assets_dir_path))
+    print(existing_files)
+    for file_ in existing_files:
+        root, file_extension = os.path.splitext(file_)
+        with (open(os.path.join(expected_assets_dir_path, file_), encoding='UTF-8') as actual_file,
+            open(f'fixtures/{root}-etalon{file_extension}', encoding='UTF-8') as etalon_file):
+            actual_content = actual_file.read()
+            etalon_content = etalon_file.read()
+            similarity = difflib.SequenceMatcher(None, actual_content, etalon_content).ratio()
+            assert similarity >= 0.99, f"Asset {file_} similarity with etalon = {similarity:.2%}. Must be >= 0.99"
