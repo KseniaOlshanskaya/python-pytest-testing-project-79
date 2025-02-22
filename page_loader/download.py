a461a31050dd299c29fd79ba89299722f9d739e1
KseniaOlshanskaya <ks14012000@gmail.com>
2025-02-23 00:36:47 +0700

    Updated download.py

--- page_loader/download.py
+++ page_loader/download.py
@@ -20,7 +20,9 @@ ASSET_TAGS = {"img": "src",
               "audio": "src",
               "source": "srcset"}
 
-HOST = 'ru.hexlet.io'
+class AssetNotFound(Exception):
+    def __init__(self, message="AssetNotFound: Asset could not be downloaded"):
+        super().__init__(message)
 
 
 def modify_name(url, extension=True):
@@ -30,47 +32,56 @@ def modify_name(url, extension=True):
     file_extension = '.html' if file_extension == '' else file_extension
     file_name = re.sub(r'^[a-zA-Z]+://', '', root)  # remove schema
     file_name = re.sub(r'[^A-Za-z0-9]', '-', file_name)  # change symbols to -
+    file_name = re.sub(r'[^A-Za-z0-9]+$', '', file_name)
+
+    logger.info(f'Local name created: {file_name}')
     if extension:
         return file_name + file_extension
     return file_name
 
 
 def download_asset(url, path_to_save):
-    logger.info(f'Downloading asset url: {url}')
-    try:
-        response = requests.get(url)
-        if response.ok:
-            logger.info(f'Server response: {response}')
-            with open(path_to_save, 'wb') as f:
-                f.write(response.content)
-    except requests.exceptions.RequestException as e:
-        logger.error(f'Cannot reach the page: {url}. Error: {e}')
+    logger.info(f'Downloading asset... {path_to_save}')
+    if len(os.path.basename(path_to_save)) > 260: # Windows limit
+        logger.info(f'Asset cannot be downloaded as its name length > 260')
+        return
+    if re.search(r'[?:*"<>\|]', os.path.basename(path_to_save)):
+        logger.info(f'Asset cannot be downloaded as its name consists of unacceptable symbols')
+        return
+    response = requests.get(url)
+    logger.info(f'Server response: {response}')
+    if response.ok:
+        with open(path_to_save, 'wb') as f:
+            f.write(response.content)
+    else:
+        raise Exception(f"Request {url} failed: {response.status_code} Reason: {response.reason}")
 
 
-def download_assets(soup, assets_dir_name):
+def download_assets(soup, assets_dir_name, host):
     logger.info('Downloading all assets')
     for tag_type, attribute in ASSET_TAGS.items():
         asset_tags = soup.find_all(tag_type)
         for tag in asset_tags:
             if tag.has_attr(attribute):  # If tag has href or src attribute
                 parsed_url = urlparse(tag[attribute])
-                if parsed_url.netloc == HOST or parsed_url.netloc == '':  # TODO Change HOST to input URL host
-                    asset_url = tag[attribute] if 'https' in tag[attribute] else ('https://' + HOST + tag[attribute])
+                if parsed_url.netloc == host or parsed_url.netloc == '' and 'login' not in parsed_url.path:
+                    asset_url = tag[attribute] if 'https' in tag[attribute] else ('https://' + host + tag[attribute])
                     asset_name = modify_name(url=asset_url)
                     full_asset_path = os.path.join(assets_dir_name, asset_name)
-                    download_asset(url=asset_url, path_to_save=full_asset_path)
+                    try:
+                        download_asset(url=asset_url, path_to_save=full_asset_path)
+                    except AssetNotFound as e:
+                        logger.info(e)
+                        continue
                     tag[attribute] = full_asset_path
     logger.info('Assets are downloaded')
 
 def download_page(url):
-    logger.info(f'Downloading page url: {url}')
-    try:
-        response = requests.get(url)
-        if response.ok:
-            logger.info(f'Server response: {response}')
-        return response
-    except requests.exceptions.RequestException as e:
-        logger.error(f'Cannot reach the page: {url}. Error: {e}')
+    response = requests.get(url)
+    logger.info(f'Server response: {response}')
+    if not response.ok:
+        raise Exception(f"Request {url} failed: {response.status_code} Reason: {response.reason}")
+    return response
 
 
 def download(url: str, output: str = None):
@@ -85,7 +96,8 @@ def download(url: str, output: str = None):
         os.mkdir(assets_dir_name)
     page = download_page(url)
     soup = BeautifulSoup(page.text, "html.parser")
-    download_assets(soup=soup, assets_dir_name=assets_dir_name)
+    parsed_url = urlparse(url)
+    download_assets(soup=soup, assets_dir_name=assets_dir_name, host=parsed_url.netloc)
     final_page = str(soup.prettify())
 
     with open(full_file_path, "w", encoding="utf-8") as file:
